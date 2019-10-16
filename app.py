9ea99ff74a191d3e2ecd9664e19863bb58fa923d
Ozkar <ozkaragoz@gmail.com>
2019-10-16 16:04:47 -0500

    fixed the html table

--- app.py
+++ app.py
@@ -1,20 +1,18 @@
 from flask import Flask, render_template, redirect
 import pymongo 
-#from pymongo import MongoClient
-#from flask_pymongo import PyMongo
+from pymongo import MongoClient
 import scrape_mars
 
-app = Flask(__name__)
+try: 
+    conn = MongoClient("mongodb://localhost:27017/") 
+    print("Connected successfully!!!") 
+except:   
+    print("Could not connect to MongoDB") 
 
-#mongo = PyMongo(app, uri="mongodb://localhost:27017/")
-myclient = pymongo.MongoClient("mongodb://localhost:27017/")
-mydb = myclient["mars_db"]
+mydb = conn["mars_db"]
 mycol = mydb["mars"]
-# client = MongoClient()
-# client = MongoClient('localhost', 27017)
-#mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_db")
-# db = client.mars_db
-# collection = db.mars
+
+app = Flask(__name__)
 
 @app.route("/")
 def index():
@@ -26,8 +24,12 @@ def scraper():
     mars = mycol
     mars_scrape = scrape_mars.scrape()
     mars.update({}, mars_scrape, upsert=True)
-
     return redirect("/", code=302)
 
 if __name__ == "__main__":
     app.run(debug=False)
+
+# Housekeeping codes Test to confirm_
+#checkdata = mycol.find() 
+#for record in checkdata: 
+#    print(record)
